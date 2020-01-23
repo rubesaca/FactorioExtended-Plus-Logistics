96d6b72065733feae917bfd6b846d6e7c7b2e4b7
billbo99 <cactus_computers-github@yahoo.com>
2020-01-23 21:32:28 +0000

    Added mk3 bots

---
+++ prototypes/technology/mk3_bots.lua
@@ -0,0 +1,3 @@
+local tech = data.raw.technology["logistic-robotics-3"]
+table.insert(tech.effects, {type = "unlock-recipe", recipe = "logistic-robot-mk3"})
+table.insert(tech.effects, {type = "unlock-recipe", recipe = "construction-robot-mk3"})
