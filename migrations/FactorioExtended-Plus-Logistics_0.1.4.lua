35fbd0d1546eaf85a35989b43e0ba03a3d53c3ba
billbo99 <cactus_computers-github@yahoo.com>
2020-03-17 15:15:29 +0000

    0.4.3

--- migrations/FactorioExtended-Plus-Logistics_0.1.4.lua
+++ migrations/FactorioExtended-Plus-Logistics_0.1.4.lua
@@ -1,27 +1,29 @@
+-- 0.4.3 .. only check the property if the technology exists
+
 for index, force in pairs(game.forces) do
-  local technologies = force.technologies
+    local technologies = force.technologies
 
-  if technologies["character-logistic-trash-slots-3"].researched then
-    force.character_trash_slot_count = 18
-  end
+    if technologies["character-logistic-trash-slots-3"] and technologies["character-logistic-trash-slots-3"].researched then
+        force.character_trash_slot_count = 18
+    end
 
-  if technologies["character-logistic-trash-slots-4"].researched then
-    force.character_trash_slot_count = 30
-  end
+    if technologies["character-logistic-trash-slots-4"] and technologies["character-logistic-trash-slots-4"].researched then
+        force.character_trash_slot_count = 30
+    end
 
-  if technologies["character-logistic-slots-7"].researched then
-    force.character_logistic_slot_count = 42
-  end
+    if technologies["character-logistic-slots-7"] and technologies["character-logistic-slots-7"].researched then
+        force.character_logistic_slot_count = 42
+    end
 
-  if technologies["character-logistic-slots-8"].researched then
-    force.character_logistic_slot_count = 48
-  end
+    if technologies["character-logistic-slots-8"] and technologies["character-logistic-slots-8"].researched then
+        force.character_logistic_slot_count = 48
+    end
 
-  if technologies["character-logistic-slots-9"].researched then
-    force.character_logistic_slot_count = 54
-  end
+    if technologies["character-logistic-slots-9"] and technologies["character-logistic-slots-9"].researched then
+        force.character_logistic_slot_count = 54
+    end
 
-  if technologies["character-logistic-slots-10"].researched then
-    force.character_logistic_slot_count = 60
-  end
+    if technologies["character-logistic-slots-10"] and technologies["character-logistic-slots-10"].researched then
+        force.character_logistic_slot_count = 60
+    end
 end
--- prototypes/technology/technology-logistic-slots.lua
+++ prototypes/technology/technology-logistic-slots.lua
@@ -1,108 +1,112 @@
-data:extend(
-    {
+-- 0.4.3 .. only add new technology if the base technology exists
+
+if data.raw.technology["character-logistic-slots"] then
+    data:extend(
         {
-            type = "technology",
-            name = "character-logistic-slots-7",
-            icon = "__base__/graphics/technology/character-logistic-slots.png",
-            icon_size = 128,
-            prerequisites = {"character-logistic-slots-6"},
-            effects = {
-                {
-                    type = "character-logistic-slots",
-                    modifier = 6
-                }
-            },
-            unit = {
-                count = 1200,
-                ingredients = {
-                    {"automation-science-pack", 1},
-                    {"logistic-science-pack", 1},
-                    {"chemical-science-pack", 1},
-                    {"production-science-pack", 1},
-                    {"utility-science-pack", 1}
+            {
+                type = "technology",
+                name = "character-logistic-slots-7",
+                icon = "__base__/graphics/technology/character-logistic-slots.png",
+                icon_size = 128,
+                prerequisites = {"character-logistic-slots-6"},
+                effects = {
+                    {
+                        type = "character-logistic-slots",
+                        modifier = 6
+                    }
                 },
-                time = 30
-            },
-            upgrade = true,
-            order = "c-k-e-g"
-        },
-        {
-            type = "technology",
-            name = "character-logistic-slots-8",
-            icon = "__base__/graphics/technology/character-logistic-slots.png",
-            icon_size = 128,
-            effects = {
-                {
-                    type = "character-logistic-slots",
-                    modifier = 6
-                }
-            },
-            prerequisites = {"character-logistic-slots-7"},
-            unit = {
-                count = 1400,
-                ingredients = {
-                    {"automation-science-pack", 1},
-                    {"logistic-science-pack", 1},
-                    {"chemical-science-pack", 1},
-                    {"production-science-pack", 1},
-                    {"utility-science-pack", 1}
+                unit = {
+                    count = 1200,
+                    ingredients = {
+                        {"automation-science-pack", 1},
+                        {"logistic-science-pack", 1},
+                        {"chemical-science-pack", 1},
+                        {"production-science-pack", 1},
+                        {"utility-science-pack", 1}
+                    },
+                    time = 30
                 },
-                time = 30
+                upgrade = true,
+                order = "c-k-e-g"
             },
-            upgrade = true,
-            order = "c-k-e-h"
-        },
-        {
-            type = "technology",
-            name = "character-logistic-slots-9",
-            icon = "__base__/graphics/technology/character-logistic-slots.png",
-            icon_size = 128,
-            effects = {
-                {
-                    type = "character-logistic-slots",
-                    modifier = 6
-                }
-            },
-            prerequisites = {"character-logistic-slots-8"},
-            unit = {
-                count = 1600,
-                ingredients = {
-                    {"automation-science-pack", 1},
-                    {"logistic-science-pack", 1},
-                    {"chemical-science-pack", 1},
-                    {"production-science-pack", 1},
-                    {"utility-science-pack", 1}
+            {
+                type = "technology",
+                name = "character-logistic-slots-8",
+                icon = "__base__/graphics/technology/character-logistic-slots.png",
+                icon_size = 128,
+                effects = {
+                    {
+                        type = "character-logistic-slots",
+                        modifier = 6
+                    }
                 },
-                time = 30
-            },
-            upgrade = true,
-            order = "c-k-e-i"
-        },
-        {
-            type = "technology",
-            name = "character-logistic-slots-10",
-            icon = "__base__/graphics/technology/character-logistic-slots.png",
-            icon_size = 128,
-            effects = {
-                {
-                    type = "character-logistic-slots",
-                    modifier = 6
-                }
-            },
-            prerequisites = {"character-logistic-slots-9"},
-            unit = {
-                count = 2000,
-                ingredients = {
-                    {"automation-science-pack", 1},
-                    {"logistic-science-pack", 1},
-                    {"chemical-science-pack", 1},
-                    {"production-science-pack", 1},
-                    {"utility-science-pack", 1}
+                prerequisites = {"character-logistic-slots-7"},
+                unit = {
+                    count = 1400,
+                    ingredients = {
+                        {"automation-science-pack", 1},
+                        {"logistic-science-pack", 1},
+                        {"chemical-science-pack", 1},
+                        {"production-science-pack", 1},
+                        {"utility-science-pack", 1}
+                    },
+                    time = 30
                 },
-                time = 30
+                upgrade = true,
+                order = "c-k-e-h"
             },
-            upgrade = true,
-            order = "c-k-e-j"
+            {
+                type = "technology",
+                name = "character-logistic-slots-9",
+                icon = "__base__/graphics/technology/character-logistic-slots.png",
+                icon_size = 128,
+                effects = {
+                    {
+                        type = "character-logistic-slots",
+                        modifier = 6
+                    }
+                },
+                prerequisites = {"character-logistic-slots-8"},
+                unit = {
+                    count = 1600,
+                    ingredients = {
+                        {"automation-science-pack", 1},
+                        {"logistic-science-pack", 1},
+                        {"chemical-science-pack", 1},
+                        {"production-science-pack", 1},
+                        {"utility-science-pack", 1}
+                    },
+                    time = 30
+                },
+                upgrade = true,
+                order = "c-k-e-i"
+            },
+            {
+                type = "technology",
+                name = "character-logistic-slots-10",
+                icon = "__base__/graphics/technology/character-logistic-slots.png",
+                icon_size = 128,
+                effects = {
+                    {
+                        type = "character-logistic-slots",
+                        modifier = 6
+                    }
+                },
+                prerequisites = {"character-logistic-slots-9"},
+                unit = {
+                    count = 2000,
+                    ingredients = {
+                        {"automation-science-pack", 1},
+                        {"logistic-science-pack", 1},
+                        {"chemical-science-pack", 1},
+                        {"production-science-pack", 1},
+                        {"utility-science-pack", 1}
+                    },
+                    time = 30
+                },
+                upgrade = true,
+                order = "c-k-e-j"
+            }
         }
-    }
-)
+    )
+end
