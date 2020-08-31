7dc7dba3d05dc16b871485b2991cd8b2ebf25a64
billbo99 <cactus_computers-github@yahoo.com>
2020-08-31 22:10:50 +0100

    icon_update

--- prototypes/item/item-logistics.lua
+++ prototypes/item/item-logistics.lua
@@ -1,44 +1,27 @@
-data:extend(
-    {
-        {
-            type = "item",
-            name = "roboport-mk2",
-            icon = "__FactorioExtended-Plus-Logistics__/graphics/icons/roboport-mk2.png",
-            icon_size = 32,
-            subgroup = "fb-logistics",
-            order = "a-a",
-            place_result = "roboport-mk2",
-            stack_size = 10
-        },
-        {
-            type = "item",
-            name = "roboport-mk3",
-            icon = "__FactorioExtended-Plus-Logistics__/graphics/icons/roboport-mk3.png",
-            icon_size = 32,
-            subgroup = "fb-logistics",
-            order = "a-b",
-            place_result = "roboport-mk3",
-            stack_size = 10
-        },
-        {
-            type = "item",
-            name = "logistic-robot-mk2",
-            icon = "__FactorioExtended-Plus-Logistics__/graphics/icons/logistic-robot-mk2.png",
-            icon_size = 32,
-            subgroup = "fb-logistics",
-            order = "b-a",
-            place_result = "logistic-robot-mk2",
-            stack_size = 50
-        },
-        {
-            type = "item",
-            name = "construction-robot-mk2",
-            icon = "__FactorioExtended-Plus-Logistics__/graphics/icons/construction-robot-mk2.png",
-            icon_size = 32,
-            subgroup = "fb-logistics",
-            order = "b-b",
-            place_result = "construction-robot-mk2",
-            stack_size = 50
-        }
-    }
-)
+local Constant = require("constant")
+
+local function CloneWithTint(source, name, subgroup, order, tint, icon_name, itype)
+    local item = table.deepcopy(data.raw[itype or "item"][source])
+    item.name = name
+    item.place_result = item.name
+    if icon_name then
+        item.icon = string.format("__FactorioExtended-Plus-Weaponry__/graphics/icons/%s", icon_name)
+    end
+    item.icons = {{icon = item.icon, icon_mipmaps = 4, icon_size = 64, tint = tint}}
+    item.subgroup = subgroup
+    item.order = order
+    data:extend({item})
+end
+
+local items = {
+    {source = "roboport", name = "roboport-mk2", subgroup = "fb-logistics", order = "a-a", tint = Constant.green_tint, itype = "item"},
+    {source = "roboport", name = "roboport-mk3", subgroup = "fb-logistics", order = "a-b", tint = Constant.blue_tint, itype = "item"},
+    {source = "logistic-robot", name = "logistic-robot-mk2", subgroup = "fb-logistics", order = "b-a", tint = Constant.green_tint, itype = "item"},
+    {source = "logistic-robot", name = "logistic-robot-mk3", subgroup = "fb-logistics", order = "b-b", tint = Constant.blue_tint, itype = "item"},
+    {source = "construction-robot", name = "construction-robot-mk2", subgroup = "fb-logistics", order = "c-a", tint = Constant.green_tint, itype = "item"},
+    {source = "construction-robot", name = "construction-robot-mk3", subgroup = "fb-logistics", order = "c-b", tint = Constant.blue_tint, itype = "item"}
+}
+
+for _, item in pairs(items) do
+    CloneWithTint(item.source, item.name, item.subgroup, item.order, item.tint, item.icon_name, item.itype)
+end
